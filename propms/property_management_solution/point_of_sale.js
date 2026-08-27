448264b909281ae602355b3d21067039f67f6e45
av-dev2 <emgani@aakvatech.com>
2026-08-27 13:14:06 +0300

    fix: rebuild the POS cost centre control on the v16 POS API

--- propms/property_management_solution/point_of_sale.js
+++ propms/property_management_solution/point_of_sale.js
@@ -1,94 +1,80 @@
-/* Customized version 2019-12-30 */
-/* Added cost_center field */
-// POS page js.
-try {
-	erpnext.pos.PointOfSale = erpnext.pos.PointOfSale.extend({
-	});
-} catch (e) { //online POS
-	class NewPOSCart extends POSCart {
-		constructor(wrapper) {
-			super(wrapper);
+// Copyright (c) 2019, Aakvatech and contributors
+// For license information, please see license.txt
+
+// Adds a Cost Center selector to the Point of Sale cart. Picking a cost centre
+// looks up the property's current lease customer and stamps the cost centre on
+// the invoice and on every item before checkout.
+frappe.provide("propms.point_of_sale");
+
+propms.point_of_sale.patch_pos = function () {
+	const ItemCart = erpnext.PointOfSale && erpnext.PointOfSale.ItemCart;
+	const Controller = erpnext.PointOfSale && erpnext.PointOfSale.Controller;
+
+	if (!ItemCart || !Controller || ItemCart.prototype.propms_cost_center_patched) {
+		return;
+	}
+	ItemCart.prototype.propms_cost_center_patched = true;
+
+	const make_customer_selector = ItemCart.prototype.make_customer_selector;
+
+	ItemCart.prototype.make_customer_selector = function () {
+		make_customer_selector.call(this);
+		this.propms_make_cost_center_field();
+	};
+
+	ItemCart.prototype.propms_make_cost_center_field = function () {
+		this.$customer_section.append(`<div class="propms-cost-center-field"></div>`);
+
+		this.cost_center_field = frappe.ui.form.make_control({
+			df: {
+				fieldtype: "Link",
+				label: __("Cost Center"),
+				fieldname: "propms_cost_center",
+				options: "Cost Center",
+				placeholder: __("Select cost center"),
+				get_query: () => ({ filters: { is_group: 0 } }),
+				onchange: () => this.propms_apply_cost_center(this.cost_center_field.get_value()),
+			},
+			parent: this.$customer_section.find(".propms-cost-center-field"),
+			render_input: true,
+		});
+		this.cost_center_field.toggle_label(false);
+	};
+
+	ItemCart.prototype.propms_apply_cost_center = function (cost_center) {
+		const frm = this.events.get_frm();
+		frm.doc.cost_center = cost_center || "";
+		(frm.doc.items || []).forEach((item) => {
+			item.cost_center = cost_center || "";
+		});
+
+		if (!cost_center) {
+			return;
 		}
-		make() {
-			this.make_dom();
-			console.log("AakvaERP POS making cost center field")
-			this.make_cost_center_field();
-			this.make_customer_field();
-			this.make_loyalty_points();
-			this.make_numpad();
-		}
-		make_cost_center_field() {
-			// console.log("AakvaERP POS making cost center field")
-			this.cost_center_field = frappe.ui.form.make_control({
-				df: {
-					fieldtype: 'Link',
-					label: 'Cost Center',
-					fieldname: 'cost_center',
-					options: 'Cost Center',
-					reqd: 0,
-					get_query: function () {
-						return {
-							filters: {
-								"is_group": 0
-							}
-						}
-					},
-					onchange: () => {
-						frappe.call({
-							method: "propms.pos.get_pos_data",
-							freeze: true,
-							args: {
-								'cost_center': this.cost_center_field.get_value()
-							}
-						}).then(r => {
-							var pos_customer = "Cash Customer"
-							if (r.message) {
-								if (r.message.hasOwnProperty('customer')) {
-									pos_customer = r.message.customer
-								}
-							}
-							this.customer_field.set_value(pos_customer);
-							this.frm.set_value('customer', pos_customer);
-							//for(var i = 0; i< r.message.lease_item.length; i++ ){
-							//	this.events.on_field_change(r.message.lease_item[i].lease_item);
-							//}
-						});
-					},
-				},
-				parent: this.wrapper.find('.customer-field'),
-				render_input: true
+
+		frappe.call({
+			method: "propms.pos.get_pos_data",
+			freeze: true,
+			args: { cost_center: cost_center },
+		}).then((r) => {
+			const customer = (r.message && r.message.customer) || "Cash Customer";
+			this.customer_field && this.customer_field.set_value(customer);
+			frm.set_value("customer", customer);
+		});
+	};
+
+	const save_and_checkout = Controller.prototype.save_and_checkout;
+
+	Controller.prototype.save_and_checkout = async function () {
+		const cost_center = this.cart && this.cart.cost_center_field && this.cart.cost_center_field.get_value();
+		if (cost_center) {
+			this.frm.doc.cost_center = cost_center;
+			(this.frm.doc.items || []).forEach((item) => {
+				item.cost_center = cost_center;
 			});
 		}
+		return save_and_checkout.call(this);
 	};
-	POSCart = NewPOSCart;
+};
 
-	class PointOfSale extends erpnext.pos.PointOfSale {
-		constructor(wrapper) {
-			super(wrapper);
-		}
-
-		submit_sales_invoice() {
-			for (var i = 0; i < this.frm.doc.items.length; i++) {
-				console.log("AakvaERP POS ", document.querySelector('input[data-fieldname="cost_center"]').value)
-				this.frm.doc.items[i].cost_center = document.querySelector('input[data-fieldname="cost_center"]').value;
-			}
-			this.frm.doc.cost_center = document.querySelector('input[data-fieldname="cost_center"]').value;
-			this.frm.savesubmit()
-				.then((r) => {
-					if (r && r.doc) {
-						this.frm.doc.docstatus = r.doc.docstatus;
-						frappe.show_alert({
-							indicator: 'green',
-							message: __(`Sales invoice ${r.doc.name} created succesfully`)
-						});
-
-						this.toggle_editing();
-						this.set_form_action();
-						this.set_primary_action_in_modal();
-					}
-				});
-		}
-
-	};
-	erpnext.pos.PointOfSale = PointOfSale;
-}
+frappe.require("point-of-sale.bundle.js", () => propms.point_of_sale.patch_pos());
