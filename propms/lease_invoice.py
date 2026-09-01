f7b24b48d23890670b0b16eb37a2d22f9bf99794
iamtalib13 <talibsheikh16@gmail.com>
2026-09-01 23:01:43 +0530

    feat(lease-invoice): generate accrued lease invoices with deferred revenue configuration

--- propms/lease_invoice.py
+++ propms/lease_invoice.py
@@ -153,6 +153,8 @@ def leaseInvoiceAutoCreate():
             ],
             order_by="parent, paid_by, invoice_item_group, date_to_invoice, currency, lease_item",
         )
+        if not lease_invoice:
+            return
         # frappe.msgprint("Lease being generated for " + str(lease_invoice))
         row_num = 1  # to identify the 1st line of the list
         prev_parent = ""
@@ -223,7 +225,25 @@ def leaseInvoiceAutoCreate():
             item_json["rate"] = invoice_item.rate
             item_json["cost_center"] = getCostCenter(invoice_item.parent)
             item_json["withholding_tax_rate"] = invoice_item.tax
-            # item_json["enable_deferred_revenue"] = 1 # Set it to true
+
+            # Fetch deferred revenue configuration from Item Master and Item Defaults / Company
+            item_doc = frappe.get_cached_doc("Item", invoice_item.lease_item)
+            if item_doc.enable_deferred_revenue:
+                item_json["enable_deferred_revenue"] = 1
+                company = frappe.get_value("Lease", invoice_item.parent, "company")
+                deferred_account = (
+                    frappe.get_cached_value(
+                        "Item Default",
+                        {"parent": invoice_item.lease_item, "company": company},
+                        "deferred_revenue_account",
+                    )
+                    or frappe.get_cached_value("Company", company, "default_deferred_revenue_account")
+                )
+                if deferred_account:
+                    item_json["deferred_revenue_account"] = deferred_account
+            else:
+                item_json["enable_deferred_revenue"] = 0
+
             item_json["service_start_date"] = str(invoice_item.schedule_start_date)
             if invoice_item.qty != int(invoice_item.qty):
                 # it means the last invoice for the lease that may have fraction of months
